af5ebf5b4bc5265fb55efb364e1499f16479ce0f
ahmed <ahmed679942@gmail.com>
2024-03-19 00:25:29 +0200

    add intent image and test it

--- app/src/main/java/com/mala/grad_project/DataSteps/StepFour.kt
+++ app/src/main/java/com/mala/grad_project/DataSteps/StepFour.kt
@@ -17,9 +17,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.*
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
+import com.mala.grad_project.R
 import com.mala.grad_project.compoableOfData.CircleOutlinePreview
 import com.mala.grad_project.compoableOfData.TwoButton
 import com.mala.grad_project.composables.Spacer150
@@ -28,42 +30,46 @@ import com.mala.grad_project.composables.Spacer30
 
 // step 4
 @Composable
+
 fun PickingPhoto(
-    onNextScreen:()->Unit,
-    onBackScreen:()->Unit
-){
+    onNextScreen: () -> Unit,
+    onBackScreen: () -> Unit
+) {
     var photoUri: Uri? by remember { mutableStateOf(null) }
     val launcher = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-        photoUri=uri
+        photoUri = uri
     }
-    val painter = rememberAsyncImagePainter(
-        ImageRequest
-            .Builder(LocalContext.current)
-            .data(data = photoUri)
-            .build()
-    )
-    Column (
+
+    val painter = if (photoUri != null) {
+        rememberAsyncImagePainter(
+            ImageRequest.Builder(LocalContext.current)
+                .data(data = photoUri)
+                .build()
+        )
+    } else {
+        painterResource(id = R.drawable.man)
+    }
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
             .padding(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally
-
-
-    ){
+    ) {
         Spacer30()
 
         CircleOutlinePreview(onclick = {
-            launcher.launch(
-                PickVisualMediaRequest(
-                    mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly
+                launcher.launch(
+                    PickVisualMediaRequest(
+                        mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly
+                    )
                 )
-            )
-        }, painter = painter)
+            }
+        , painter = painter)
+
         Spacer150()
         Spacer150()
-        TwoButton(onNextScreen = { onNextScreen() }, onBackScreen = {  onBackScreen()}, text = "Finish")
-
-
+        TwoButton(onNextScreen = { onNextScreen() }, onBackScreen = { onBackScreen() }, text = "Finish")
     }
 }
